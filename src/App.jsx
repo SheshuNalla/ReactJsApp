a12dee299ec9c904e56319c17bd74c5055836b82
SheshuNalla <sheshunalla002@gmail.com>
2026-05-27 13:43:57 +0530

    react hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
-import { useState } from 'react'
+import { use } from 'react';
+import { useState, useEffect, useActionState } from 'react'
 
 
 
-const Card = ({title}) => {
+const Card = ({title , rating , isCool}) => {
+  const [count , setCount] = useState(0);
   const [hasLiked , setHasLiked] = useState(false);
 
+  useEffect(() => {
+    console.log(`${title} has been liked ${hasLiked}`)
+  },[hasLiked])
+
   return (
-    <div className='card'>
-      <h2>{title}</h2>
+    <div className='card' onClick={()=> setCount(count+1)}>
+      <h2>{title} <br/> {count || null}</h2>
       <button onClick={()=>setHasLiked(!hasLiked)}>
         {hasLiked ? "💖" : "🤍"}
       </button>
+      <h2>rating: {rating}</h2>
     </div>
     
   )
@@ -20,9 +27,9 @@ const App = () => {
   return (
     <div className='card-container'>
       
-      <Card title = "dhurandar" />
-      <Card title = "pushpa" />
-      <Card title = "dangal" />
+      <Card title = "dhurandar" rating={5} />
+      <Card title = "pushpa" rating={4} />
+      <Card title = "dangal" rating={3} />
     </div>
   )
 }
