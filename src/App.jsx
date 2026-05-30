de2abbb67bd8dc23c2c09b24a6611e997c0aa612
SheshuNalla <sheshunalla002@gmail.com>
2026-05-30 13:05:13 +0530

    feat(dataset): add movies dataset

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,12 @@
-import React, { useState, useEffect } from "react"
+import React, { useState } from "react"
 import Search from "./components/Search"
 
-const API_BASE_URL = `http://www.omdbapi.com/?apikey=[${API_KEY}]`
-const API_KEY = import.meta.env.VITE_OMDB_API_KEY;
-
-const API_OPTIONS = {
-  method : "GET",
-  headers : {
-    accept: 'application/json',
-    Authorization: `Bearer ${API_KEY}`
-  }
-}
 
 const App = () => {
   const [searchTerm , setSearchTerm] = useState("");
-  const[errorMsg, setErrorMsg] = useState("")
 
-  const fetchMovies = async() => {
-    try{
-      const endpoint = `${API_BASE_URL}/discover/movies?sort_by=popularity.desc`;
-
-      const response = await fetch(endpoint, API_OPTIONS);
-
-      alert(response)
-
-    }
-    catch(error){
-      console.error(`Error fetching movies: ${error}`)
-      setErrorMsg("Failed to fetch movies. Please try again later.")
-
-    }
-  }
-
-  useEffect(() => {
-    fetchMovies();
-  }, [])
+  
+  
 
   return (
     <main>
